f2e225baa5ecf44dfb37797c8148e56ff608b10f
Derek Schwantner <derek@Dereks-Mac-mini.local>
2021-11-03 00:14:28 -0400

    created home endpoint that logs message

---
+++ index.js
@@ -0,0 +1,9 @@
+// import server 
+const server = require('./api/server');
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log(`server is running on ${port}`)
+});
+
